45dc7ee4650942a88422cdafdee696cf200dc332
Marija Stopa <mstopa@griddynamics.com>
2026-03-05 10:41:24 +0100

    Simplify function to hello world for debugging

--- function/src/main.py
+++ function/src/main.py
@@ -2,5 +2,5 @@ import functions_framework
 
 @functions_framework.http
 def main(request):
-    """HTTP Cloud Function."""
+    """HTTP Cloud Function - Simple test."""
     return 'Hello from Cloud Functions!', 200
