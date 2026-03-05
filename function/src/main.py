1c7781699c9cde9d86b28fc0c00f84450a064344
Marija Stopa <mstopa@griddynamics.com>
2026-03-05 11:02:40 +0100

    Switch to Flask app

--- function/src/main.py
+++ function/src/main.py
@@ -1,2 +1,9 @@
-def main(request):
-    return 'Hello World!', 200
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return 'Hello from Flask!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
